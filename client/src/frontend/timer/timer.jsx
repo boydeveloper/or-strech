5c7eb0cfe36dd2e64725b1213f5d4b6b88477be8
boy developer <daniel.onazi116@gmail.com>
2023-10-22 19:59:19 +0100

    design update: new stretch screen

--- client/src/frontend/timer/timer.jsx
+++ client/src/frontend/timer/timer.jsx
@@ -1,73 +1,85 @@
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import style from "./timer.module.css";
-import { useState } from "react";
+
 function Timer() {
-  const name = localStorage.getItem("username");
   const [isActive, setIsActive] = useState(true);
 
-  const toggleActive = () => {
+  const toggleInterval = () => {
     setIsActive(!isActive);
   };
-  return (
-    <div className={style.timerOverview}>
-      <div className={style.timer__wrapper}>
-        <div className={style.timer_panel}>
-          <div
-            className={isActive ? style.active_position : ""}
-            onClick={toggleActive}
-          >
-            <ion-icon name="man"></ion-icon>
-            <p>Standing</p>
-          </div>
-          <div
-            className={!isActive ? style.active_position : ""}
-            onClick={toggleActive}
-          >
-            <span class="material-symbols-outlined">
-              airline_seat_recline_normal
-            </span>
-            <p>Seated</p>
-          </div>
-          <div>
-            <h1>00</h1>
-            <span>Set Reminder Interval</span>
-            <div className={style.timer_switches}>
-              <button>
-                <ion-icon name="caret-up"></ion-icon>
-              </button>
-              <button>
-                <ion-icon name="caret-down"></ion-icon>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className={style.timer_box}>
-          <div className={style.time}>
-            <div className={style.minutes}>
-              <h1>90</h1>
-              <span>Minutes</span>
-            </div>
-            :
-            <div className={style.seconds}>
-              <h1>00</h1>
-              <span>Seconds</span>
-            </div>
-          </div>
-          <button>START!</button>
-        </div>
-        <div className={style.go_box}>
-          <div>
-            <h1>
-              START <br /> STRETCHING <br />
-              <span>NOW!</span>
-            </h1>
-          </div>
-          <button>
-            <span>GO!</span>
-          </button>
-        </div>
+  const [isTimerActive, setIsTimerActive] = useState(false);
 
-        <button className={style.pauseButton}>STOP/PAUSE</button>
+  const toggleActive = () => {
+    setIsTimerActive(!isTimerActive);
+  };
+
+  const activeClass = isTimerActive ? style.active : "";
+  return (
+    <div className={style.timer__overview}>
+      <div className={style.timer__wrapper}>
+        <div className={style.timer}>
+          <div className={style.reminder_interval}>
+            <div className={style.reminder_buttons}>
+              <button className={style.reminder_button_up}></button>
+              <button className={style.reminder_button_down}></button>
+            </div>
+            <div
+              className={
+                isActive ? `${style.standing} ${style.active}` : style.standing
+              }
+              onClick={toggleInterval}
+            >
+              standing
+            </div>
+            <div
+              className={
+                !isActive ? `${style.seated} ${style.active}` : style.seated
+              }
+              onClick={toggleInterval}
+            >
+              seated
+            </div>
+            <div className={style.reminder_display}>
+              888
+              <div className={style.interval_value}>90</div>
+            </div>
+            <p className={style.interval_text}>Set reminder interval</p>
+          </div>
+          <div className={style.timer_display}>
+            <div className={style.timer_box}>
+              <div className={style.time}>
+                <div className={style.minutes}>
+                  888
+                  <div className={style.min}>90</div>
+                  <span>Minutes</span>
+                </div>
+                <div className={style.dots}>:</div>
+                <div className={style.seconds}>
+                  88
+                  <div className={style.sec}>00</div>
+                  <span>Seconds</span>
+                </div>
+              </div>
+            </div>
+            <div
+              className={`${style.timer_options} ${activeClass}`}
+              onClick={toggleActive}
+            >
+              <div className={style.counting_img}></div>
+              <button className={style.reset_button}>Reset</button>
+              <button className={style.start_button}></button>
+            </div>
+          </div>
+        </div>
+        <div className={style.start_watching}>
+          <h1 className={style.start_message}>
+            Start <br />
+            stretching <br />
+            <span>Now!</span>
+          </h1>
+          <button className={style.go_button}></button>
+        </div>
+        <button className={style.stop_btn}>STOP/PAUSE</button>
       </div>
     </div>
   );
