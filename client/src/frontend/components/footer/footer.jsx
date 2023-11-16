4032579cc1f45883d8aa5d47cb1e80e54e3fcc89
boy developer <daniel.onazi116@gmail.com>
2023-11-16 14:45:32 +0100

    tweaks

--- client/src/frontend/components/footer/footer.jsx
+++ client/src/frontend/components/footer/footer.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import style from "./footer.module.css";
 
 function Footer() {
   const [currentTime, setCurrentTime] = useState(getFormattedTime());
   const [dotColor, setDotColor] = useState("#000");
+  const [cityInitials, setCityInitials] = useState("");
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -15,6 +15,16 @@ function Footer() {
       setDotColor(getDotColor(seconds));
     }, 1000);
 
+    // Get user's time zone
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    // Get user's city initials from the time zone
+    const cityArray = userTimeZone.split("/");
+    if (cityArray.length === 2) {
+      const cityName = cityArray[1].replace(/_/g, " ");
+      setCityInitials(cityName.toUpperCase());
+    }
+
     return () => clearInterval(intervalId);
   }, []);
 
@@ -22,7 +32,7 @@ function Footer() {
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes();
-    const ampm = hours >= 12 ? "  PM" : "   AM";
+    const ampm = hours >= 12 ? " PM" : " AM";
     const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
     const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
 
@@ -39,16 +49,16 @@ function Footer() {
   return (
     <div className={style.footerWrapper}>
       <div className={style.footer}>
-        <p>&copy;OR-Stretch</p>
-        <span>
-          {currentTime}
+        <p>&copy;OR-Stretch {new Date().getFullYear()}</p>
 
+        <span>
           <div
             className={style.time_dot}
             style={{
               background: dotColor,
             }}
           ></div>
+          {currentTime}, {cityInitials && `${cityInitials}`}.
         </span>
       </div>
     </div>
